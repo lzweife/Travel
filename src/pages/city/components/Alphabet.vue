20534fbdd751fef0365f364cb8542f6d1412d5df
linzhiwei <1140607841@qq.com>
2019-05-03 00:32:47 +0800

    城市选择布局和better-scroll滚动

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="list">
+    <ul>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style scoped lang="stylus">
+  @import "~styles/varibles.styl"
+  .list
+    display flex
+    flex-direction column
+    justify-content center
+    position absolute
+    width .4rem
+    top 1.58rem
+    right 0
+    bottom 0
+    .item
+      line-height .4rem
+      color $bgColor
+      text-align center
+</style>
